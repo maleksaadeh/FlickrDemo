94727b2e2dee30e1f58a841bf9e52b899b22ccdc
Malek Saadeh <saadehmalek94@gmail.com>
2024-11-19 10:37:49 -0500

    Initial Commit

---
+++ FlickrDemo/FlickrDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrDemoApp.swift
+//  FlickrDemo
+//
+//  Created by Malek Saadeh on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
